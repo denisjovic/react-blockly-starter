1ba415a90549e8f717122942aa0fe071c842cec3
denisjovic <redzistrejsn@pm.me>
2022-07-08 18:22:21 +0200

    Add categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BlocklyWorkspace } from 'react-blockly';
 import Blockly from 'blockly'
 import './App.css'
+import { DEFAULT_TOOLBOX } from './defaultToolbox';
 
 function App() {
 
@@ -19,7 +20,7 @@ function App() {
   Blockly.Blocks['lights_on'] = {
     init: function() {
       this.appendDummyInput()
-          .appendField("turn on the light");
+          .appendField("Turn on the light");
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
       this.setColour(105);
@@ -45,50 +46,16 @@ function App() {
     `<xml xmlns="http://www.w3.org/1999/xhtml">
       <block type="text" x="70" y="30">
         <field name="TEXT">
-        CHANGE ME!
       </field>
       </block>
     </xml>
     `;
 
-  type CustomToolboxCategory = 'VARIABLE' | 'PROCEDURE'
   
-  interface ToolboxBlock {
-    type: string,
-    colour?: string,
-    shadow?: string,
-  }
-
-  interface ToolboxCategories  {
-    name: string,
-    custom?: CustomToolboxCategory,
-    blocks: ToolboxBlock[]
-
-
-  }
-  const TOOLBOX_CATEGORIES: any = {
-    "kind": "flyoutToolbox",
-    "contents": [
-      {
-        "kind": "block",
-        "type": "lights_off",
-      },
-      {
-        "kind": "block",
-        "type": "lights_on",
-      },
-      {
-        "kind": "block",
-        "type": "switch",
-      }
-    ]
-
-    
-  }
 
   return (
     <div className="App">
-      <h1>Blockly</h1>
+      <h1>Blockly Starter Code</h1>
       <BlocklyWorkspace
       className="fill-height"
       initialXml={initialXml}
@@ -100,7 +67,7 @@ function App() {
           snap: true,
         },
       }}
-      toolboxConfiguration={TOOLBOX_CATEGORIES}
+      toolboxConfiguration={DEFAULT_TOOLBOX}
     />
     </div>
   )
